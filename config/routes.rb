a9bfc7caac44ea073e305b11f40f9897323a47e5
Ben Ruiz <bneruiz@me.com>
2019-11-13 16:32:50 -0700

    two models working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root "departments#index"  
   # sets index from departments_controller as the root route
 
-  resources :departments
+  get '/departments', to: 'departments#index', as: 'all_the_departments'
+  get '/department/:id', to: 'departments#show', as: 'the_one_department'
+  delete '/department/:id', to: 'departments#destroy', as: 'destroy_the_department'
+
+  resources :departments do 
+    resources :items
+  end
   # creates seven different routes in your application, all mapping to the departments_controller:
 end
